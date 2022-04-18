2595e9667cf810c8fae7a0f92822eaaeffd8c227
Alex Geis <siegxela@gmail.com>
2022-04-18 13:08:31 -0600

    updates to resolvers, typedefs, added context file (not updated)

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { User, Subscription } = require("../models");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find({});
+    },
+    subscriptions: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Subscription.find(params);
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+    createSubscription: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+    updateUser: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+    updateSubscription: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    password: String!
+    email: String!
+  }
+
+  type Subscription {
+    _id: ID!
+    subscriptionName: String!
+    monthlyCost: Int,
+    annualCost: Int,
+    paymentType: String,
+    startDate: Date,
+    dueDate: Date,
+    autoPay: Boolean,
+    autoRenew: Boolean,
+  }
+
+  type Query {
+    user: User
+    # matchups(_id: String): [Matchup]
+  }
+
+  type Mutation {
+    createUser(username: String!, password: String!, email: String!): User
+    updateUser(_id: ID!username: String!, password: String!, email: String!): User
+    removeUser(_id: ID!)
+
+    createSubscription(password: String!, email: String!): User
+    updateSubscription(_id: ID!, password: String!, email: String!): User
+  }
+`;
+
+module.exports = typeDefs;
