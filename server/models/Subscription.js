4832975d3789b854c068491503a39216847dcb48
Alex Geis <siegxela@gmail.com>
2022-04-18 13:59:47 -0600

    updated subcription model to properly export

--- server/models/Subscription.js
+++ server/models/Subscription.js
@@ -1,21 +1,21 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const subscriptionModel = new Schema({
-    subscriptionName: {
-        type: String, 
-        required: true,
-    },
-    // A subscription should only have ONE of the following: Monthly Cost OR Annual Cost
-    monthlyCost: Number,
-    annualCost: Number,
-    paymentType: String,
-    startDate: Date,
-    // We might want dueDate as a Integer, representing the Day-of-Month
-    dueDate: {type: Number, min: 1, max: 31},
-    autoPay: {type: Boolean, default: false},
-    autoRenew: {type: Boolean, default: false}
-})
+  subscriptionName: {
+    type: String,
+    required: true,
+  },
+  // A subscription should only have ONE of the following: Monthly Cost OR Annual Cost
+  monthlyCost: Number,
+  annualCost: Number,
+  paymentType: String,
+  startDate: Date,
+  // We might want dueDate as a Integer, representing the Day-of-Month
+  dueDate: { type: Number, min: 1, max: 31 },
+  autoPay: { type: Boolean, default: false },
+  autoRenew: { type: Boolean, default: false },
+});
 
-const Subscription = model('Subscription', subscriptionModel)
+const Subscription = model("Subscription", subscriptionModel);
 
-module.export = Subscription
+module.exports = Subscription;
