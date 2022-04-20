d2cfbb71aa68fb98ef33028c8f1fb63f89369abd
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-19 19:36:42 -0600

    fixing apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,47 @@
 import "./App.css";
 import AppContainer from "./components/AppContainer";
-<<<<<<< HEAD
 // import NewSub from "./components/pages/NewSub/NewSub"
-=======
 import { UserProvider } from "./components/utils/UserContext";
->>>>>>> 9c608857b0d86cf03084ad9a50cc0c9b1b2a78f8
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { setContext } from "@apollo/client/link/context";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <UserProvider>
-      <div className="App">
-        <AppContainer />
-      </div>
-    </UserProvider>
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <div className="App">
+          <AppContainer />
+        </div>
+      </UserProvider>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -12,7 +12,7 @@ import AuthService from "../../utils/auth";
 function Home() {
   // const [loginInfo, setloginInfo] = useState("");
   const [formState, setFormState] = useState({ userName: "", password: "" });
-  // const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,15 +26,15 @@ function Home() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(formState);
-    // try {
-    //   const { data } = await login({
-    //     variables: { ...formState },
-    //   });
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
 
-    //   AuthService.login(data.login.token); //json web token
-    // } catch (event) {
-    //   console.error(event);
-    // }
+      AuthService.login(data.login.token); //json web token
+    } catch (event) {
+      console.error(event);
+    }
 
     // clear form values
     setFormState({
@@ -48,7 +48,7 @@ function Home() {
   return (
     <>
       <Container>
-      <div className="card"/>
+        <div className="card" />
         <img src={Money} className="img-fluid" id="money" />
         <div id="logo">
           <img src={Logo3} className="img-fluid" id="logo" alt="Logo" />
