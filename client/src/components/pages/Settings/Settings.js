c918179ba21a5daeaab0b051510a9a114966847e
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-20 13:22:31 -0600

    updating settings page

--- client/src/components/pages/Settings/Settings.js
+++ client/src/components/pages/Settings/Settings.js
@@ -1,17 +1,46 @@
-import WelcomeScreen from "../Welcome/Welcome";
-import { Container } from "react-bootstrap";
+import Welcome from "../Welcome/Welcome";
+import { Container, ButtonGroup, ToggleButton, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Settings/settings.css";
+import React, { useState } from "react";
 
-function SettingsPage() {
-    return (
-        <Container>
-        <h1 style={styles.heading}>Settings</h1>
-        <p style={styles.button}>Place Holder User</p>
-        <p>Place Holder Password</p>
-        <ButtonGroup>
+function Settings() {
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState("1");
+
+  const radios = [
+    { name: "Light Mode", value: "1" },
+    { name: "Dark Mode", value: "2" },
+  ];
+
+  const styles = {
+    card: {
+      margin: 20,
+      background: "#e8eaf6",
+    },
+    heading: {
+      background: "#3f51b5",
+      minHeight: 50,
+      lineHeight: 3.5,
+      fontSize: "1.2rem",
+      color: "white",
+      padding: "0 20px",
+    },
+    content: {
+      padding: 20,
+    },
+    button: {
+      background: "#00FF00",
+    },
+  };
+  return (
+    <Container>
+      <h1 style={styles.heading}>Settings</h1>
+      <p style={styles.button}>Place Holder User</p>
+      <p>Place Holder Password</p>
+      <ButtonGroup>
         {radios.map((radio, idx) => (
-            <ToggleButton
+          <ToggleButton
             key={idx}
             id={`radio-${idx}`}
             type="radio"
@@ -20,25 +49,27 @@ function SettingsPage() {
             value={radio.value}
             checked={radioValue === radio.value}
             onChange={(e) => setRadioValue(e.currentTarget.value)}
-            >
+          >
             {radio.name}
-            </ToggleButton>
+          </ToggleButton>
         ))}
-        </ButtonGroup>
-        <p>Place Holder Name</p>
-        <div className="mb-2">
+      </ButtonGroup>
+      <p>Place Holder Name</p>
+      <div className="mb-2">
         <Button variant="primary" size="lg">
-            Home
+          Home
         </Button>{" "}
         <Button variant="danger" size="lg">
-            Logout
+          Logout
         </Button>
         <div>
-        <a id="buttonInstall" role="button">Install!</a>
+          <a id="buttonInstall" role="button">
+            Install!
+          </a>
         </div>
-        </div>
+      </div>
     </Container>
-    )
+  );
 }
 
-export default SettingsPage;
+export default Settings;
