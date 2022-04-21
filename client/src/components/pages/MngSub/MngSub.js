77609a3a29a114b271d96bfdb75362ffe65d5f2f
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-21 15:10:57 -0600

    commenting out code in MngSub.js

--- client/src/components/pages/MngSub/MngSub.js
+++ client/src/components/pages/MngSub/MngSub.js
@@ -6,89 +6,94 @@ import { useQuery } from "@apollo/client";
 import { QUERY_SINGLE_USER } from "../../utils/queries";
 import AuthService from "../../utils/auth";
 
-<<<<<<< HEAD
-const MngSub = () => {
-  //   if (!subscriptions.length) {
-  //     return <h3>No Subscriptions Yet</h3>;
-  //   }
-  let userId = AuthService.getProfile().data._id;
+// const MngSub = () => {
+//   //   if (!subscriptions.length) {
+//   //     return <h3>No Subscriptions Yet</h3>;
+//   //   }
+//   let userId = AuthService.getProfile().data._id;
 
-  const { loading, error, data } = useQuery(QUERY_SINGLE_USER, {
-    variables: { userId: userId },
-  });
+//   const { loading, error, data } = useQuery(QUERY_SINGLE_USER, {
+//     variables: { userId: userId },
+//   });
 
-  if (loading) {
-    return <div> ...Loading </div>;
-  }
+//   if (loading) {
+//     return <div> ...Loading </div>;
+//   }
 
+//   return (
+//     <div className="container-fluid">
+//       <h1>Manage My Subscriptions</h1>
+//       <div className="card-body">
+//         <div className="row justify-content-around">
+//           <div className="col-4">
+//             <h3>All Subscriptions</h3>
+//             <ul>
+//               <li>
+//                 <SubscriptionList subscriptions={data.user.subscriptions} />
+//               </li>
+//             </ul>
+//           </div>
+//           <div className="col-4">
+//             <h3>Monthly Costs</h3>
+//             <ul>
+//               <li>Render Cost</li>
+//             </ul>
+//           </div>
+//           <h3>Total Annual Cost =</h3>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MngSub;
+
+function MngSub() {
   return (
-    <div className="container-fluid">
-      <h1>Manage My Subscriptions</h1>
-      <div className="card-body">
-        <div className="row justify-content-around">
-          <div className="col-4">
-            <h3>All Subscriptions</h3>
-            <ul>
-              <li>
-                <SubscriptionList subscriptions={data.user.subscriptions} />
-              </li>
-            </ul>
-          </div>
-          <div className="col-4">
-            <h3>Monthly Costs</h3>
-            <ul>
-              <li>Render Cost</li>
-            </ul>
-          </div>
-          <h3>Total Annual Cost =</h3>
-        </div>
-      </div>
+    <div class="container-fluid">
+      <h1 id="mngSubPage">Manage My Subscriptions</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">A/M</th>
+            <th colspan="2">Subscription Name</th>
+            <th scope="col">Monthly Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">M</th>
+            <td colspan="2">
+              <button id="mngSubBtn">LA Fitness</button>
+            </td>
+            <td>$</td>
+          </tr>
+          <tr>
+            <th scope="row">A</th>
+            <td colspan="2">
+              <button id="mngSubBtn">Prime</button>
+            </td>
+            <td>$</td>
+          </tr>
+          <tr>
+            <th scope="row">A</th>
+            <td colspan="2">
+              <button id="mngSubBtn">Hulu</button>
+            </td>
+            <td>$</td>
+          </tr>
+        </tbody>
+      </table>
+      <h5 id="totalCost">Total Monthly Cost = </h5>
+      <h5 id="totalCost">Total Annual Cost = </h5>
     </div>
   );
-};
-
-export default MngSub;
-=======
-function MngSub() {
-    return (
-<div class="container-fluid">
-<h1 id='mngSubPage'>Manage My Subscriptions</h1>
-<table class="table table-striped">
-<thead>
-    <tr>
-    <th scope="col">A/M</th>
-    <th colspan="2">Subscription Name</th>
-    <th scope="col">Monthly Cost</th>
-    </tr>
-</thead>
-<tbody>
-    <tr>
-    <th scope="row">M</th>
-    <td colspan="2"><button id='mngSubBtn'>LA Fitness</button></td>
-    <td>$</td>
-    </tr>
-    <tr>
-    <th scope="row">A</th>
-    <td colspan="2"><button id='mngSubBtn'>Prime</button></td>
-    <td>$</td>
-    </tr>
-    <tr>
-    <th scope="row">A</th>
-    <td colspan="2"><button id='mngSubBtn'>Hulu</button></td>
-    <td>$</td>
-    </tr>
-</tbody>
-</table>
-<h5 id='totalCost'>Total Monthly Cost = </h5>
-<h5 id='totalCost'>Total Annual Cost = </h5>
-</div>
-);
 }
 
 export default MngSub;
 
-
-{/* <h1 id='mngSubPage'>Manage My Subscriptions</h1>    
+{
+  /* <h1 id='mngSubPage'>Manage My Subscriptions</h1>    
 <div class="row justify-content-around" id='mngSubBox'>
     <div className="col-4" id='allSubList' >
     <h5>Subscription</h5>
@@ -103,5 +108,5 @@ export default MngSub;
     </ul> 
     </div>
     <h5 id='totalCost'>Total Annual Cost = </h5>
-</div> */}
->>>>>>> 08446c73392cf4539f2fb4a120bbb19f55f76006
+</div> */
+}
