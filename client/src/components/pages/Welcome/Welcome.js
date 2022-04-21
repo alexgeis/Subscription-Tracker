b1b539b979ff27bb3f5ba0c907463636cf961605
Maria Paterno <mariapaterno7@yahoo.com>
2022-04-21 13:30:20 -0600

    settings button gone now?

--- client/src/components/pages/Welcome/Welcome.js
+++ client/src/components/pages/Welcome/Welcome.js
@@ -34,11 +34,6 @@ function Welcome() {
   return (
     <>
       <div className="container-fluid">
-        <Link to="/settings">
-          <button id="settingsButton" className="btn btn-primary" type="button">
-            Settings
-          </button>
-        </Link>
         <h1 id="welcomeScreen">Welcome {data.user.username}</h1>
         <Link to="/managesub">
           <button id="addSubBtn" className="btn btn-primary" type="button">
