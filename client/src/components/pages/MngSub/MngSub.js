e62b68ff5f332d615cf965cf7390c63a6a2cb75f
Maria Paterno <mariapaterno7@yahoo.com>
2022-04-21 14:43:22 -0600

    mngsub page done (if we keep it)

--- client/src/components/pages/MngSub/MngSub.js
+++ client/src/components/pages/MngSub/MngSub.js
@@ -3,28 +3,56 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function MngSub() {
     return (
-
-        <div class="container-fluid">
-<h1>Manage My Subscriptions</h1>    
-<div className="card-body">
-<div class="row justify-content-around">
-    <div className="col-4">
-        <h3>All Subscriptions</h3>
-        <ul>
-    <li>Render Subscriptions</li>
-    </ul>
-    </div>
-    <div className="col-4">
-    <h3>Monthly Costs</h3>
-    <ul>
-    <li>Render Cost</li>
-    </ul> 
-    </div>
-    <h3>Total Annual Cost =</h3>
-</div>
-</div>
+<div class="container-fluid">
+<h1 id='mngSubPage'>Manage My Subscriptions</h1>
+<table class="table table-striped">
+<thead>
+    <tr>
+    <th scope="col">A/M</th>
+    <th colspan="2">Subscription Name</th>
+    <th scope="col">Monthly Cost</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+    <th scope="row">M</th>
+    <td colspan="2"><button id='mngSubBtn'>LA Fitness</button></td>
+    <td>$</td>
+    </tr>
+    <tr>
+    <th scope="row">A</th>
+    <td colspan="2"><button id='mngSubBtn'>Prime</button></td>
+    <td>$</td>
+    </tr>
+    <tr>
+    <th scope="row">A</th>
+    <td colspan="2"><button id='mngSubBtn'>Hulu</button></td>
+    <td>$</td>
+    </tr>
+</tbody>
+</table>
+<h5 id='totalCost'>Total Monthly Cost = </h5>
+<h5 id='totalCost'>Total Annual Cost = </h5>
 </div>
 );
 }
 
 export default MngSub;
+
+
+{/* <h1 id='mngSubPage'>Manage My Subscriptions</h1>    
+<div class="row justify-content-around" id='mngSubBox'>
+    <div className="col-4" id='allSubList' >
+    <h5>Subscription</h5>
+    <ul>
+    <li>Render Subscriptions</li>
+    </ul>
+    </div>
+    <div className="col-4">
+    <h5>Monthly Cost</h5>
+    <ul>
+    <li>Render Cost</li>
+    </ul> 
+    </div>
+    <h5 id='totalCost'>Total Annual Cost = </h5>
+</div> */}
