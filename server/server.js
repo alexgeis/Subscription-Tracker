3239b4627293260d7648a79fc8d11a0dce863164
Alex Geis <siegxela@gmail.com>
2022-04-22 14:52:34 -0600

    heroku fix 4

--- server/server.js
+++ server/server.js
@@ -6,9 +6,7 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.MONGODB_URI || 3001;
-// const PORT = process.env.PORT || 3001;
-// const PORT = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 const server = new ApolloServer({
