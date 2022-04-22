54383b7faeb722735df2f72cdcf48b531b255c6b
Alex Geis <siegxela@gmail.com>
2022-04-22 15:20:42 -0600

    heroku 4

--- server/server.js
+++ server/server.js
@@ -19,7 +19,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
+  // app.use(express.static(path.join(__dirname, "../client/build")));
+
   app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
 }
 
 const startApolloServer = async (typeDefs, resolvers) => {
