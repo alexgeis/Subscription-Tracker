680eb1941257b21d4dfcaf66f55b5ad576f9eeb4
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-21 19:47:57 -0600

    creating edit modal, update singleSub

---
+++ client/src/components/EditComponent.js
@@ -0,0 +1,55 @@
+import { Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
+
+const EditModal = (s) => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div>
+      <Button variant="primary" size="lg" onClick={() => setShowModal(true)}>
+        Edit
+      </Button>
+      <Modal
+        size="lg"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update {s.subscriptionName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control type="input" placeholder={s.subscriptionName} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Cost:</Form.Label>
+              <Form.Control type="input" placeholder={s.monthlyCost} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Payment Type:</Form.Label>
+              <Form.Control type="input" placeholder={s.paymentType} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Auto Pay:</Form.Label>
+              <Form.Control type="input" placeholder={s.autoPay} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Auto Renew:</Form.Label>
+              <Form.Control type="input" placeholder={s.autoRenew} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default EditModal;
--- client/src/components/pages/SingleSub/SingleSub.js
+++ client/src/components/pages/SingleSub/SingleSub.js
@@ -1,87 +1,83 @@
-import '../SingleSub/singleSub.css'
+import "../SingleSub/singleSub.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button }  from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
 import { useQuery } from "@apollo/client";
-import { useParams } from "react-router-dom"
-import { QUERY_SINGLE_USER } from "../../utils/queries"
-import AuthService from "../../utils/auth"
-
+import { useParams } from "react-router-dom";
+import { QUERY_SINGLE_USER } from "../../utils/queries";
+import { useState } from "react";
+import AuthService from "../../utils/auth";
+import EditModal from "../../EditComponent";
 
 function SingleSub() {
+  // set modal display state
 
-    let userId = (AuthService.getProfile().data._id)
-    const params = useParams();
-    const subId = params.id;
-    const { loading, error, data } = useQuery(QUERY_SINGLE_USER, {
-        variables: { userId: userId }
-      })
-    
-      if (loading){
-        return (<div> ...Loading </div>)
-      }
+  let userId = AuthService.getProfile().data._id;
+  const params = useParams();
+  const subId = params.id;
+  const { loading, error, data } = useQuery(QUERY_SINGLE_USER, {
+    variables: { userId: userId },
+  });
 
-      const subscriptions = data.user.subscriptions
+  if (loading) {
+    return <div> ...Loading </div>;
+  }
 
-      const subIndex = subscriptions.findIndex((object) => {
-          return object._id === subId
-      })
+  const subscriptions = data.user.subscriptions;
 
-      const s = subscriptions[subIndex]
+  const subIndex = subscriptions.findIndex((object) => {
+    return object._id === subId;
+  });
 
-    return (
-    
-<div className="container-fluid">     
-    <h1>{s.subscriptionName}</h1>
-        
+  const s = subscriptions[subIndex];
 
-    <div className="container">
+  return (
+    <div className="container-fluid">
+      <h1>{s.subscriptionName}</h1>
 
-
-    <div>
-        <p>Monthly Cost:</p>
-        <p>${s.monthlyCost}.00</p>
-    </div>
-    <div>
-        <p>Annual Cost:</p>
-        <p>${s.annualCost}.00</p>
-    </div>
-    <div>
-        <p>Payment Type:</p>
-        <p>{s.paymentType}</p>
-    </div>
-    <div>
+      <div className="container">
+        <div>
+          <p>Monthly Cost:</p>
+          <p>${s.monthlyCost}.00</p>
+        </div>
+        <div>
+          <p>Annual Cost:</p>
+          <p>${s.annualCost}.00</p>
+        </div>
+        <div>
+          <p>Payment Type:</p>
+          <p>{s.paymentType}</p>
+        </div>
+        {/* <div>
         <p>Start Date:</p>
         <p>{s.startDate}</p>
     </div>
     <div>
         <p>Next Due Date:</p>
         <p>05/{s.dueDate}/22</p>
-    </div>
-    <div>
-        <p>Auto Pay?</p>
-        <p>{s.autoPay}</p>
-    </div>
-    <div>
-        <p>Auto Renew?</p>
-        <p>{s.autoRenew}</p>
-    </div>
-<div>
-
-    <div className="mb-2">
-        <Button variant="primary" size="lg">
-            Edit
-        </Button>{" "}
-        <Button variant="danger" size="lg">
-            Delete
-        </Button>
+    </div> */}
+        <div>
+          <p>Auto Pay?</p>
+          <p>{s.autoPay ? "Yes" : "No"}</p>
         </div>
+        <div>
+          <p>Auto Renew?</p>
+          <p>{s.autoRenew ? "Yes" : "No"}</p>
+        </div>
+        <div>
+          <div className="mb-2">
+            <EditModal subscription={s} />{" "}
+            <Button variant="danger" size="lg">
+              Delete
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-);
+  );
 }
 
-{/* <InputGroup className="mb-3">
+{
+  /* <InputGroup className="mb-3">
     <InputGroup.Text>$</InputGroup.Text>
     <FormControl aria-label="Amount (to the nearest dollar)" />
     <InputGroup.Text>.00</InputGroup.Text>
@@ -96,7 +92,7 @@ function SingleSub() {
     <InputGroup>
     <InputGroup.Text>Description</InputGroup.Text>
     <FormControl as="textarea" aria-label="With textarea" />
-</InputGroup> */}
-
+</InputGroup> */
+}
 
 export default SingleSub;
