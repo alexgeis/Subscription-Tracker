13bb28b90f47e0fd9a64ea88f510fceb41affccf
Alex Geis <siegxela@gmail.com>
2022-04-24 12:33:22 -0600

    updated validation messages and removed console logger

--- client/src/components/EditComponent.js
+++ client/src/components/EditComponent.js
@@ -4,24 +4,17 @@ import { useMutation } from "@apollo/client";
 import { UPDATE_SUBSCRIPTION } from "./utils/mutations";
 import AuthService from "./utils/auth";
 
-
-const EditModal = ({subscription}) => {
-
+const EditModal = ({ subscription }) => {
   const [showModal, setShowModal] = useState(false);
-  const [subName, setSubName] = useState(subscription.subscriptionName)
-  const [monthlyCost, setMonthlyCost] = useState(subscription.monthlyCost)
-  const [annualCost, setAnnualCost] = useState(subscription.annualCost)
-  const [payType, setPayType] = useState(subscription.paymentType)
+  const [subName, setSubName] = useState(subscription.subscriptionName);
+  const [monthlyCost, setMonthlyCost] = useState(subscription.monthlyCost);
+  const [annualCost, setAnnualCost] = useState(subscription.annualCost);
+  const [payType, setPayType] = useState(subscription.paymentType);
   const [autoPay, setAutoPay] = useState(subscription.autoPay);
   const [autoRenew, setRenew] = useState(subscription.autoRenew);
-  const [updateSubscription, {error}] = useMutation(UPDATE_SUBSCRIPTION);
+  const [updateSubscription, { error }] = useMutation(UPDATE_SUBSCRIPTION);
 
-
-  let userId = (AuthService.getProfile().data._id)
-  function consoleLogger(event){
-    event.preventDefault()
-    console.log(subName, monthlyCost, annualCost, payType, autoPay, autoRenew)
-  }
+  let userId = AuthService.getProfile().data._id;
 
   const handleInputChange = (e) => {
     const { target } = e;
@@ -29,50 +22,50 @@ const EditModal = ({subscription}) => {
     const inputValue = target.value;
 
     switch (inputType) {
-      case 'sName': {
-        setSubName(inputValue)
+      case "sName": {
+        setSubName(inputValue);
         break;
       }
-      case 'sCost': {
-        setMonthlyCost(parseInt(inputValue))
-        setAnnualCost((parseInt(inputValue))*12)
+      case "sCost": {
+        setMonthlyCost(parseInt(inputValue));
+        setAnnualCost(parseInt(inputValue) * 12);
         break;
       }
-      case 'sPayType': {
-        setPayType(inputValue)
+      case "sPayType": {
+        setPayType(inputValue);
         break;
       }
     }
-  }
+  };
 
   const handleCheckboxChange = (e) => {
     const { target } = e;
     const inputType = target.id;
 
     switch (inputType) {
-      case 'sAutoPay': {
-        setAutoPay(target.checked)
+      case "sAutoPay": {
+        setAutoPay(target.checked);
         break;
       }
-      case 'sAutoRenew': {
-        setRenew(target.checked)
+      case "sAutoRenew": {
+        setRenew(target.checked);
         break;
       }
     }
-  }
+  };
 
   const handleHide = () => {
-    setAutoPay(subscription.autoPay)
-    setRenew(subscription.autoRenew)
-    setMonthlyCost(subscription.monthlyCost)
-    setAnnualCost(subscription.annualCost)
-    setSubName(subscription.subscriptionName)
-    setPayType(subscription.paymentType)
-    setShowModal(false)
-  }
-  
+    setAutoPay(subscription.autoPay);
+    setRenew(subscription.autoRenew);
+    setMonthlyCost(subscription.monthlyCost);
+    setAnnualCost(subscription.annualCost);
+    setSubName(subscription.subscriptionName);
+    setPayType(subscription.paymentType);
+    setShowModal(false);
+  };
+
   const handleFormSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
       const { data } = await updateSubscription({
@@ -84,21 +77,21 @@ const EditModal = ({subscription}) => {
           paymentType: payType,
           autoPay: autoPay,
           autoRenew: autoRenew,
-        }
-      })
+        },
+      });
     } catch (error) {
-      console.log(JSON.stringify(error))
+      console.log(JSON.stringify(error));
     } finally {
-      handleHide()
+      handleHide();
     }
-  }
- 
+  };
+
   return (
     <div>
       <Button variant="primary" size="md" onClick={() => setShowModal(true)}>
         Edit
       </Button>
-      
+
       <Modal
         size="lg"
         show={showModal}
@@ -112,61 +105,60 @@ const EditModal = ({subscription}) => {
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>Name:</Form.Label>
-              <Form.Control 
+              <Form.Control
                 type="input"
                 id="sName"
-                onChange={handleInputChange} 
-                placeholder={subscription.subscriptionName} 
+                onChange={handleInputChange}
+                placeholder={subscription.subscriptionName}
               />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Monthly Cost:</Form.Label>
-              <Form.Control 
+              <Form.Control
                 type="input"
                 id="sCost"
-                onChange={handleInputChange} 
-                placeholder={subscription.monthlyCost} 
+                onChange={handleInputChange}
+                placeholder={subscription.monthlyCost}
               />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Payment Type:</Form.Label>
-              <Form.Control 
+              <Form.Control
                 type="input"
                 id="sPayType"
-                onChange={handleInputChange} 
-                placeholder={subscription.paymentType} 
+                onChange={handleInputChange}
+                placeholder={subscription.paymentType}
               />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Auto Pay:</Form.Label>
               <Form.Check
-                inline 
+                inline
                 type="checkbox"
                 id="sAutoPay"
                 defaultChecked={subscription.autoPay}
-                onChange = {handleCheckboxChange} 
+                onChange={handleCheckboxChange}
               />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Auto Renew:</Form.Label>
               <Form.Check
-                inline 
+                inline
                 type="checkbox"
                 id="sAutoRenew"
                 defaultChecked={subscription.autoRenew}
-                onChange = {handleCheckboxChange}  
+                onChange={handleCheckboxChange}
               />
             </Form.Group>
           </Form>
-          <Button variant="primary" size="sm" onClick = {consoleLogger}>
-            Console Logger
-          </Button>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleHide}>
             Cancel
           </Button>
-          <Button variant="primary" onClick={handleFormSubmit}>Save changes</Button>
+          <Button variant="primary" onClick={handleFormSubmit}>
+            Save changes
+          </Button>
         </Modal.Footer>
       </Modal>
     </div>
--- client/src/components/pages/SignUp/SignUp.js
+++ client/src/components/pages/SignUp/SignUp.js
@@ -4,18 +4,21 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { CREATE_USER } from "../../utils/mutations";
 import AuthService from "../../utils/auth";
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert } from "react-bootstrap";
 
 const SignUp = () => {
-
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [createUser, {error}] = useMutation(CREATE_USER)
+  const [createUser, { error }] = useMutation(CREATE_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
-  }
+  };
   if (error) {
     console.error(JSON.stringify(error.message));
   }
@@ -28,14 +31,13 @@ const SignUp = () => {
       event.preventDefault();
       event.stopPropagation();
     }
-console.log(userFormData)
-      try {
-  console.log("Working?")      
-      const {data} = await createUser({ 
-        variables: {...userFormData }})
-          
+    console.log(userFormData);
+    try {
+      console.log("Working?");
+      const { data } = await createUser({
+        variables: { ...userFormData },
+      });
 
-    
       console.log(data);
       AuthService.newUser(data.createUser.token);
     } catch (error) {
@@ -44,68 +46,87 @@ console.log(userFormData)
     }
 
     setUserFormData({
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     });
   };
 
-
   return (
     <>
-    <div className="container-fluid">
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your signup!
-        </Alert>
-        <Form.Group>
-          <h1 id="signUp"> Sign Up</h1>
-          <Form.Label htmlFor='username'>Username</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your username'
-            name='username'
-            onChange={handleInputChange} 
-            value={userFormData.username}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Username is required</Form.Control.Feedback>
-        </Form.Group>
+      <div className="container-fluid">
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          {/* show alert if server response is bad */}
+          <Alert
+            dismissible
+            onClose={() => setShowAlert(false)}
+            show={showAlert}
+            variant="danger"
+          >
+            Something went wrong with your signup!
+          </Alert>
+          <Form.Group>
+            <h1 id="signUp"> Sign Up</h1>
+            <Form.Label htmlFor="username">Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your username (must be at least 6 characters)"
+              name="username"
+              onChange={handleInputChange}
+              value={userFormData.username}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Username is required
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Your email address'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required</Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Your email address"
+              name="email"
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Email is required
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required</Form.Control.Feedback>
-        </Form.Group>
-        <Button id="signUpBtn"
-          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
-      </div><div id="whiteSpace"></div>
+          <Form.Group>
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Your password (must be at least 6 characters)"
+              name="password"
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Password is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            id="signUpBtn"
+            disabled={
+              !(
+                userFormData.username &&
+                userFormData.email &&
+                userFormData.password
+              )
+            }
+            type="submit"
+            variant="success"
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <div id="whiteSpace"></div>
     </>
   );
 };
