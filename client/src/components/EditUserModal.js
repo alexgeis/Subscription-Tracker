f278106e1ed0bfa13bb4ce1856a4b7fe481e06a2
Austin A <austinandrews89@gmail.com>
2022-04-30 12:10:54 -0600

    Added new component for editing users, will be a modal like our edit subscription

---
+++ client/src/components/EditUserModal.js
@@ -0,0 +1,13 @@
+import { Modal } from 'react-bootstrap';
+import { UseState } from 'react';
+import { UseMutation } from '@apollo/client';
+
+
+function EditUser(props){
+    return (
+        <>
+        </>
+    )
+}
+
+export default EditUser;
