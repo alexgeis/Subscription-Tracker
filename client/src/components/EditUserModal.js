d916437941f15b3c66c7552d54f135328c4487cb
Austin A <austinandrews89@gmail.com>
2022-04-30 12:21:05 -0600

    Built out skeleton of component, with necessary state variables, placeholder functions, and some imports

--- client/src/components/EditUserModal.js
+++ client/src/components/EditUserModal.js
@@ -1,9 +1,23 @@
 import { Modal } from 'react-bootstrap';
-import { UseState } from 'react';
-import { UseMutation } from '@apollo/client';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import UPDATE_USER from './utils/mutations'
 
 
 function EditUser(props){
+    const [showModal, setShowModal] = useState(false);
+    const [userName, setUserName] = useState(`props here`);
+    const [userEmail, setUserEmail] = useState(`props here`);
+    const [userPassword, setUserPassword] = useState(`props here`);
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <>
         </>
