786303cd8db7d0265c894167f7b09d0a16bce73a
Austin A <austinandrews89@gmail.com>
2022-04-30 13:24:06 -0600

    Added front-end validation for username and password

--- client/src/components/EditUserModal.js
+++ client/src/components/EditUserModal.js
@@ -11,23 +11,62 @@ function EditUser({user}){
     const [userName, setUserName] = useState(user.username);
     const [userEmail, setUserEmail] = useState(user.email);
     const [userPassword, setUserPassword] = useState(false);
+    const [userPasswordConfirm, setUserPasswordConfirm] = useState(false);
 
     const handleHide = () => {
+        setUserName(user.username);
+        setUserEmail(user.email);
+        setUserPassword(false);
         setShowModal(false);
     }
 
     const handleInputChange = (e) => {
-        e.preventDefault();
-    }
+        const { target } = e;
+        const inputType = target.id;
+        const inputValue = target.value;
+
+        switch (inputType) {
+            case "uName": {
+                setUserName(inputValue)
+                break
+            }
+            case "uEmail": {
+                setUserEmail(inputValue)
+                break
+            }
+            case "uPass": {
+                setUserPassword(inputValue)
+                break
+            }
+            case "uPassConfirm": {
+                setUserPasswordConfirm(inputValue)
+                break
+            }
+        }
+    };
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
+        if (userName.length < 6){
+            return alert("Username must be at least 6 characters")
+        }
+        if ((userPassword)){
+            if (userPassword.length < 6){
+                return alert ("Password must be at least 6 characters")
+            }
+            if (userPassword !== userPasswordConfirm){
+                return alert ("Password and Confirm Password do not match")
+            }
+        }
+
+        
     }
 
     const consoleLogger = (e) => {
         e.preventDefault()
-        console.log(user)
-        console.log(user.email)
+        console.log(userName)
+        console.log(userEmail)
+        console.log(userPassword)
     }
 
     return (
@@ -70,11 +109,12 @@ function EditUser({user}){
                             <Form.Label>Confirm Password: </Form.Label>
                             <Form.Control
                                 type = "password"
-                                id = "uPassConfirm">
+                                id = "uPassConfirm"
+                                onChange = {handleInputChange}>
                             </Form.Control>
                         </Form.Group>
                     </Form>
-                    <Button variant="primary">
+                    <Button variant="primary" onClick={handleFormSubmit}>
                         Submit
                     </Button>
                 </Modal.Body>
